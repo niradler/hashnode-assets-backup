825c820a4dc979d34be07055e6820fb9d2b78a65
Nir Adler <niradler55@gmail.com>
2022-12-08 22:06:51 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # hashnode-assets-backup
-hashnode assets backup
+
+Hashnode backup posts to GitHub but assets is linked to cdn.hashnode.com, this action will download the assets and add them to the repo
+
+## Inputs
+
+### `output_path`
+
+## Example usage
+
+```yaml
+uses: actions/hashnode-assets-backup@v0.1
+with:
+  output_path: "assets"
+```
--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
+const Url = require("url");
+const glob = require("glob");
+
+let outputPath = "assets";
+
+const downloads = (urls) => {
+  outputPath = core.getInput("output_path") || outputPath;
+  console.log(`Output folder: ${outputPath}!`);
+  [...new Set(urls)].forEach((url) => {
+    const fullPath = `./${outputPath}${Url.parse(url).path}`;
+
+    fs.mkdirSync(path.dirname(fullPath), { recursive: true });
+    console.log("folder created");
+    const temp = fs.createWriteStream(fullPath);
+    https.get(url, function (response) {
+      response.pipe(temp);
+      temp.on("finish", () => {
+        temp.close();
+        console.log(`${fullPath} downloaded`);
+      });
+    });
+  });
+};
 
 try {
-    const outputPath = core.getInput('output_path');
-    console.log(`Output folder: ${outputPath}!`);
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+
+  glob("**.md", function (er, files) {
+    files.forEach((filePath) => {
+      try {
+        console.log(`found ${filePath}`);
+        const file = fs.readFileSync(filePath, "utf-8");
+        const urls = [...file.match(/https{0,1}:\/\/(cdn)\S*/g)];
+
+        downloads(urls);
+      } catch (error) {
+        console.log(error.message);
+      }
+    });
+  });
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
